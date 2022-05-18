5a21d6441fb07f5b0eac0e672efe31223ddcdf0a
ziyobiddinov <ziyobiddinovjamshidbek@gmail.com>
2022-05-19 01:44:31 +0500

    script commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var name = prompt("Ismingizni kiriting");
+var userName = prompt("Familiyangizni kiriting");
+var userAge = prompt("Yoshingizni kiriting");
+var study = prompt("O'qiysizmi?");
+var job = prompt("Kasbingiz nima?");
+var interest = prompt("Qiziqishingiz");
+
+
+console.log("Ismi: " + name + "\n" + "Familiya: " + userName + "\n" + "Yoshi: " + userAge + "\n" + "Univer: " + study +"\n" + "Kasbi: " + job + "\n" + "Qiziqishi: " + interest);
